acf25b8c85401d1071ab25203d983f1fcd1999dc
Apdullah Yayık <apdullahyayik@gmail.com>
2020-01-03 22:39:51 +0300

    Update word_tokenize.py

--- word_tokenize.py
+++ word_tokenize.py
@@ -1,3 +1,5 @@
+import re
+
 def word_tokenize(_sentence):
     """
     Splits given sentence into words using regex.
@@ -10,6 +12,4 @@ def word_tokenize(_sentence):
         are evaluated as words with considering Turkish semantics.
 
         """
-
-    word_pattern = re.compile(r"\w{2,15}' ?\w{1,4}|[\w{1,2}./-]+|[\d.,]+|\w+|[.,!?;:]")
-    return word_pattern.findall(str(_sentence))
+    return re.compile(r"\w{2,15}' ?\w{1,4}|[\w{1,2}./-]+|[\d.,]+|\w+|[.,!?;:]").findall(str(_sentence))
