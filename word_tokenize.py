86c420f028a25a5ba69e98855ecc94f91f57dc06
Apdullah Yayık <apdullahyayik@gmail.com>
2020-01-12 19:43:57 +0300

    Update word_tokenize.py
    each rule is organized in line with Turkish gramer

--- word_tokenize.py
+++ word_tokenize.py
@@ -1,15 +1,29 @@
 import re
 
-def word_tokenize(_sentence):
+def word_tokenize(sentence):
     """
-    Splits given sentence into words using regex.
+    
 
     Args:
-        _sentence(str): a sentence.
+        sentence (str): any sentence.
 
     Returns:
-        list: word of given sentence. Some punctations (., !?; : )
-        are evaluated as words with considering Turkish semantics.
+        list: each item is a word.
 
-        """
-    return re.compile(r"\w{2,15}' ?\w{1,4}|[\w{1,2}./-]+|[\d.,]+|\w+|[.,!?;:]").findall(str(_sentence))
+    """
+    
+    
+    acronym_each_dot = r"(?:[a-zğçşöüı]\.){2,}"
+    acronym_end_dot = r"\b[a-zğçşöüı]{2,3}\."
+    suffixes = r"[a-zğçşöüı]{3,}' ?[a-zğçşöüı]{0,3}"
+    numbers = r"\d+[.,:\d]+"
+    any_word = r"[a-zğçşöüı]+"
+    punctuations = r"[a-zğçşöüı]*[.,!?;:]"
+    
+    word_regex = "|".join([acronym_each_dot,
+                           acronym_end_dot,
+                           suffixes,
+                           numbers,
+                           any_word,
+                           punctuations])
+    return re.compile("%s"%word_regex, re.I).findall(sentence)
